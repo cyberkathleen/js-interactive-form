8dd9c07f4d7c140e17f733e2c510f2c8fe89b6f9
Kathleen <67641959+cyberkathleen@users.noreply.github.com>
2025-01-23 17:05:26 +0100

    Update "T-Shirt Info" selection

--- js/script.js
+++ js/script.js
@@ -2,6 +2,9 @@
 const nameField = document.getElementById('name');
 const otherJobField = document.getElementById('other-job-role');
 const jobRoleField = document.getElementById('title');
+const designSelect = document.getElementById('design');
+const colorSelect = document.getElementById('color');
+const colorOptions = colorSelect.children;
 
 // Set focus on the "Name" field by default
 nameField.focus();
@@ -15,5 +18,29 @@ jobRoleField.addEventListener('change', e => {
     otherJobField.style.display = 'block';
   } else {
     otherJobField.style.display = 'none';
+  }
+});
+
+// Disable the "Color" dropdown by default
+colorSelect.disabled = true;
+
+// Listen for changes in the "Design" dropdown and enable the "Color" dropdown with available colors for the selected design
+designSelect.addEventListener('change', e => {
+  const selectedDesign = e.target.value;
+  colorSelect.disabled = false;
+
+  // Loop trough color options and show/hide option based on the selected design
+  for (let i = 0; i < colorOptions.length; i++) {
+    const colorTheme = colorOptions[i].getAttribute('data-theme');
+
+    if (colorTheme === selectedDesign) {
+      colorOptions[i].hidden = false;
+      colorOptions[i].disabled = false;
+      colorOptions[i].selected = true;
+    } else {
+      colorOptions[i].hidden = true;
+      colorOptions[i].disabled = true;
+      colorOptions[i].selected = false;
+    }
   }
 });
